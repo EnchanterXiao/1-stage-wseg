53bad0106a432d3a2351fd999c513eafd4723696
Enchanter_Xiao <991784084@qq.com>
2021-10-10 16:25:14 +0800

    'wgap_v3'

--- models/stage_net.py
+++ models/stage_net.py
@@ -7,6 +7,7 @@ from .CAM_CASA_WGAP import *
 from .CAM_MF import *
 from .CAM_MF_v2 import *
 from .CAM_CASA_WGAP_v2 import *
+from .CAM_CASA_WGAP_v3 import *
 
 #
 # Dynamic change of the base class
@@ -32,6 +33,8 @@ def network_factory(cfg):
         return network_CAM_MF_v2(cfg)
     elif cfg.MODEL == 'CAM_CASA_WGAP_v2':
         return network_CAM_CASA_WGAP_v2(cfg)
+    elif cfg.MODEL == 'CAM_CASA_WGAP_v3':
+        return network_CAM_CASA_WGAP_v3(cfg)
     else:
         raise NotImplementedError("Unknown model '{}'".format(cfg.MODEL))
 
